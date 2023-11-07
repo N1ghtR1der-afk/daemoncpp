107fd44f317d1210aaa68de195f0e9eb41720b0a
Palachovcorp <palachovcorp@mail.ru>
2023-11-07 22:14:26 +0300

    123

--- main.cpp
+++ main.cpp
@@ -16,7 +16,20 @@ void get_paths(vector<string>& paths, const string& current_path) {
 			get_paths(paths, file.path().string());
 		}
 		else {
-           
+    filesystem::path sourceFile = file.path();
+    filesystem::path targetParent = "./encrypted";
+
+    auto target = targetParent / sourceFile.filename(); 
+
+    try 
+    {
+        filesystem::create_directories(targetParent); 
+        filesystem::copy_file(sourceFile, target, filesystem::copy_options::overwrite_existing);
+    }
+    catch (std::exception& e) 
+    {
+        std::cout << e.what();
+    }
 		}
 	}
 	
@@ -29,19 +42,7 @@ int main() {
 	get_paths(paths, "./test");
 	cout << "the files were placed in another folder" << endl;
 	
-	fs::path sourceFile = "path/to/sourceFile.ext";
-    fs::path targetParent = "path/to/target";
-    auto target = targetParent / sourceFile.filename(); // sourceFile.filename() returns "sourceFile.ext".
 
-    try // If you want to avoid exception handling, then use the error code overload of the following functions.
-    {
-        fs::create_directories(targetParent); // Recursively create target directory if not existing.
-        fs::copy_file(sourceFile, target, fs::copy_options::overwrite_existing);
-    }
-    catch (std::exception& e) // Not using fs::filesystem_error since std::bad_alloc can throw too.  
-    {
-        std::cout << e.what();
-    }
 	return 0;
 }
 
