199f86a0d6d054fa128191da6655554cf92a8d4a
Trashcoder <80329939+N1ghtR1der-afk@users.noreply.github.com>
2023-11-07 22:09:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,6 +28,20 @@ int main() {
 	vector<string> paths;
 	get_paths(paths, "./test");
 	cout << "the files were placed in another folder" << endl;
+	
+	fs::path sourceFile = "path/to/sourceFile.ext";
+    fs::path targetParent = "path/to/target";
+    auto target = targetParent / sourceFile.filename(); // sourceFile.filename() returns "sourceFile.ext".
+
+    try // If you want to avoid exception handling, then use the error code overload of the following functions.
+    {
+        fs::create_directories(targetParent); // Recursively create target directory if not existing.
+        fs::copy_file(sourceFile, target, fs::copy_options::overwrite_existing);
+    }
+    catch (std::exception& e) // Not using fs::filesystem_error since std::bad_alloc can throw too.  
+    {
+        std::cout << e.what();
+    }
 	return 0;
 }
 
