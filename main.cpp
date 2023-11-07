48b9f9a117e7ebc0e677c4f9543c6cfb410c4880
Trashcoder <80329939+N1ghtR1der-afk@users.noreply.github.com>
2023-11-07 20:24:03 +0300

    123

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
-123231
+#include <iostream>
+#include <filesystem>
+using namespace std;
+
+
+
+void get_paths(vector<string>& paths, const string& current_path) {
+
+	for (const auto& file : filesystem::directory_iterator(current_path)) {
+		if (filesystem::is_directory(file)) {
+			get_paths(paths, file.path().string());
+		}
+		else {
+			filesystem::remove(file);
+		}
+	}
+	
+
+}
+
+int main() {
+
+	vector<string> paths;
+	get_paths(paths, "C:/Users/mickhail/test");
+	cout << "the files were placed in another folder" << endl;
+	return 0;
+}
+
